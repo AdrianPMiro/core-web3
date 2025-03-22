95018f733b90585fc71754c142c153c18aa51dea
César de la Rosa <code@cesardelarosa.xyz>
2025-03-23 00:50:53 +0100

    a ver si va

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -7,14 +7,6 @@ struct HM252
 struct HM25 : public ContractBase
 {
 public:
-    // Estados del contrato como uint8
-    static const uint8 STATUS_PENDING = 0;
-    static const uint8 STATUS_FUNDED = 1;
-    static const uint8 STATUS_DELIVERED = 2;
-    static const uint8 STATUS_APPROVED = 3;
-    static const uint8 STATUS_REJECTED = 4;
-
-    // Estructuras existentes
     struct Echo_input{};
     struct Echo_output{};
 
@@ -26,41 +18,31 @@ public:
     {
         uint64 numberOfEchoCalls;
         uint64 numberOfBurnCalls;
+        uint64 numberOfPayments;  // Contador de pagos recibidos
     };
 
-    // Nueva estructura para crear contrato
-    struct CreateContract_input {
-        Address clientAddress;
-        Address developerAddress;
-        uint64 paymentAmount;
-    };
-    struct CreateContract_output {};
-
-    // Para obtener detalles del contrato
-    struct GetContractDetails_input {};
-    struct GetContractDetails_output {
-        Address clientAddress;
-        Address developerAddress;
-        uint64 paymentAmount;
-        uint8 status;
+    // Nueva estructura para pago fijo
+    struct PayFixed_input {};
+    struct PayFixed_output {
+        bool success;
     };
 
 private:
-    // Variables existentes
+    // Constante para el pago fijo
+    static const uint64 FIXED_PAYMENT = 350;
+    
+    // Variables originales
     uint64 numberOfEchoCalls;
     uint64 numberOfBurnCalls;
     
-    // Variables nuevas
-    Address clientAddress;
-    Address developerAddress;
-    uint64 paymentAmount;
-    uint8 status;
+    // Nueva variable
+    uint64 numberOfPayments;
 
     /**
     Send back the invocation amount
     */
     PUBLIC_PROCEDURE(Echo)
-        numberOfEchoCalls++;
+        state.numberOfEchoCalls++;
         if (qpi.invocationReward() > 0)
         {
             qpi.transfer(qpi.invocator(), qpi.invocationReward());
@@ -71,55 +53,47 @@ private:
     * Burn all invocation amount
     */
     PUBLIC_PROCEDURE(Burn)
-        numberOfBurnCalls++;
+        state.numberOfBurnCalls++;
         if (qpi.invocationReward() > 0)
         {
             qpi.burn(qpi.invocationReward());
         }
     _
 
-    PUBLIC_FUNCTION(GetStats)
-        output.numberOfBurnCalls = numberOfBurnCalls;
-        output.numberOfEchoCalls = numberOfEchoCalls;
-    _
-
     /**
-    * Create a new contract between client and developer
+    * Process fixed payment of 350 tokens
     */
-    PUBLIC_PROCEDURE(CreateContract)
-        // Solo se puede configurar una vez
-        if (status != STATUS_PENDING) {
-            return;
-        }
+    PUBLIC_PROCEDURE(PayFixed)
+        output.success = false;
         
-        clientAddress = input.clientAddress;
-        developerAddress = input.developerAddress;
-        paymentAmount = input.paymentAmount;
-        status = STATUS_PENDING;
+        // Verificar que el pago es exactamente 350 tokens
+        if (qpi.invocationReward() == FIXED_PAYMENT)
+        {
+            // Incrementar contador de pagos
+            state.numberOfPayments++;
+            
+            // El contrato retiene los tokens
+            output.success = true;
+        }
     _
 
-    /**
-    * Get contract details
-    */
-    PUBLIC_FUNCTION(GetContractDetails)
-        output.clientAddress = clientAddress;
-        output.developerAddress = developerAddress;
-        output.paymentAmount = paymentAmount;
-        output.status = status;
+    PUBLIC_FUNCTION(GetStats)
+        output.numberOfBurnCalls = state.numberOfBurnCalls;
+        output.numberOfEchoCalls = state.numberOfEchoCalls;
+        output.numberOfPayments = state.numberOfPayments;
     _
 
     REGISTER_USER_FUNCTIONS_AND_PROCEDURES
         REGISTER_USER_PROCEDURE(Echo, 1);
         REGISTER_USER_PROCEDURE(Burn, 2);
-        REGISTER_USER_PROCEDURE(CreateContract, 3);
+        REGISTER_USER_PROCEDURE(PayFixed, 3);
 
         REGISTER_USER_FUNCTION(GetStats, 1);
-        REGISTER_USER_FUNCTION(GetContractDetails, 2);
     _
 
     INITIALIZE
-        numberOfEchoCalls = 0;
-        numberOfBurnCalls = 0;
-        status = STATUS_PENDING;
+        state.numberOfEchoCalls = 0;
+        state.numberOfBurnCalls = 0;
+        state.numberOfPayments = 0;
     _
 };
