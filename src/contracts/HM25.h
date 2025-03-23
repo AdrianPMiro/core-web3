275804583b1678d672d6616ad1018f669a5b8db4
César de la Rosa <code@cesardelarosa.xyz>
2025-03-23 02:47:18 +0100

    hm 25 es cool

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -18,11 +18,25 @@ public:
     {
         uint64 numberOfEchoCalls;
         uint64 numberOfBurnCalls;
+        uint64 numberOfPayments;  // Contador de pagos recibidos
+    };
+
+    // Nueva estructura para pago fijo
+    struct PayFixed_input {};
+    struct PayFixed_output {
+        bool success;
     };
 
 private:
+    // Constante para el pago fijo
+    static const uint64 FIXED_PAYMENT = 350;
+    
+    // Variables originales
     uint64 numberOfEchoCalls;
     uint64 numberOfBurnCalls;
+    
+    // Nueva variable
+    uint64 numberOfPayments;
 
     /**
     Send back the invocation amount
@@ -46,15 +60,33 @@ private:
         }
     _
 
+    /**
+    * Process fixed payment of 350 tokens
+    */
+    PUBLIC_PROCEDURE(PayFixed)
+        output.success = false;
+        
+        // Verificar que el pago es exactamente 350 tokens
+        if (qpi.invocationReward() == FIXED_PAYMENT)
+        {
+            // Incrementar contador de pagos
+            state.numberOfPayments++;
+            
+            // El contrato retiene los tokens
+            output.success = true;
+        }
+    _
+
     PUBLIC_FUNCTION(GetStats)
         output.numberOfBurnCalls = state.numberOfBurnCalls;
         output.numberOfEchoCalls = state.numberOfEchoCalls;
+        output.numberOfPayments = state.numberOfPayments;
     _
 
     REGISTER_USER_FUNCTIONS_AND_PROCEDURES
-
         REGISTER_USER_PROCEDURE(Echo, 1);
         REGISTER_USER_PROCEDURE(Burn, 2);
+        REGISTER_USER_PROCEDURE(PayFixed, 3);
 
         REGISTER_USER_FUNCTION(GetStats, 1);
     _
@@ -62,5 +94,6 @@ private:
     INITIALIZE
         state.numberOfEchoCalls = 0;
         state.numberOfBurnCalls = 0;
+        state.numberOfPayments = 0;
     _
 };
